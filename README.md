16febee0c382001a29d6a99a47e3da076919518f
James Hu <hello@james.hu>
2015-06-08 21:28:50 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# sublime-toggle-rspec-focus
+
+A Sublime Text plugin that comes in handy when working with RSpec. 
+
+Often you will find yourself adding `:focus` to test specific examples. This plugin makes it easy by allowing you to toggle `:focus` on a specific line with a keyboard shortcut.
---
+++ toggle_rspec_focus.py
@@ -0,0 +1,24 @@
+import sublime, sublime_plugin, re
+
+class ToggleRSpecFocus(sublime_plugin.TextCommand):
+    def run(self, edit, surplus=False):
+        for region in self.view.sel():
+            line = self.view.line(region)
+            line_contents = self.view.substr(line)
+
+            focus_regex = r'(?:it|describe|context)\s(?:.+?)(?:\"|\')(\,\s\:focus)(.+)do'
+            focus_match = re.search(focus_regex, line_contents)
+
+            # If :focus is found, remove it
+            if focus_match:
+                line_without_focus = re.sub(focus_match.group(1), "", line_contents)
+                self.view.replace(edit, line, line_without_focus)
+
+            # Otherwise, add it
+            else:
+                vanilla_regex = r'(\s*(?:it|describe|context)\s(?:.+?)(?:\"|\'))(\,?.+)do'
+                vanilla_match = re.search(vanilla_regex, line_contents)
+
+                line_with_focus = vanilla_match.group(1) + ", :focus" + vanilla_match.group(2) + "do"
+
+                self.view.replace(edit, line, line_with_focus)
